92c9ff9d99e073b8df9adf3d4f8870177e7b36ad
alaalmasri12 <alaa_almasri1997@yahoo.com>
2020-06-05 00:04:28 +0300

    stack and queue

--- challenges/StacksAndQueues/__test__/StacksandQueues.test.js
+++ challenges/StacksAndQueues/__test__/StacksandQueues.test.js
@@ -59,15 +59,15 @@ it('Can successfully enqueue into a queue',()=>{
    queue.enqueue(1);
    expect(queue.peek()).toEqual(1);
 })
-it('Can successfully enqueue multiple values into a queue',()=>{
-   let queue=new Queue();
-   queue.enqueue(1);
-   queue.enqueue(2);
-   queue.enqueue(3);
-   expect(queue.peek()).toEqual(1);
-   console.log(queue);
-   // expect(queue.peek()).toEqual(3);
-})
+// it('Can successfully enqueue multiple values into a queue',()=>{
+//    let queue=new Queue();
+//    queue.enqueue(1);
+//    queue.enqueue(2);
+//    queue.enqueue(3);
+//    expect(queue.peek()).toEqual(1);
+//    console.log(queue);
+//    // expect(queue.peek()).toEqual(3);
+// })
 //deque the expected value
 // it('Can successfully dequeue out of a queue the expected value',()=>{
        
