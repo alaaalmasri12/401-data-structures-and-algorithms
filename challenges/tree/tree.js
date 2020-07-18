8d4d0204cc4edb1ac6326177312f0118cb0a8c7a
alaalmasri12 <alaa_almasri1997@yahoo.com>
2020-07-19 02:47:33 +0300

    fixing some bugs

--- challenges/tree/tree.js
+++ challenges/tree/tree.js
@@ -1,165 +1,105 @@
-'use strict';
-const Node = require('./node');
-class BinaryTree {
-    constructor(root) {
-        this.root = root;
-    }
-    preorder() {
-        let results = [];
-        let _move = (node) => {
-            results.push(node.value);
-            if (node.left) _move(node.left);
-            if (node.right) _move(node.right);
-        }
-        _move(this.root);
-        return results
+class Node {
 
+    constructor(data, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+class BinaryTree {
+    constructor(value) {
+        this.root = new Node(value);
+        this.counter = 0;
+    }
+    insert(value) {
+
+        this.counter++;
+        let newNode = new Node(value);
+        const _add = (node) => {
+            if (value < node.data) {
+                if (!node.left) {
+                    node.left = newNode;
+                }
+                else {
+                    _add(node.left);
+                }
+            }
+            else if (value > node.data) {
+                if (!node.right) {
+                    node.right = newNode;
+                }
+                else {
+                    _add(node.right);
+                }
+            }
+        }
+        _add(this.root);
+    }
+    contain(value) {
+        let curent = this.root;
+        while (curent) {
+            if (curent.data === value) {
+                return true;
+            }
+            else if (curent.data < value) {
+                curent = curent.left;
+            }
+            else if (curent.data > value) {
+                curent = curent.right;
+            }
+        }
+        return false;
+    }
+    preoder() {
+        let results = [];
+        const _walk = (node) => {
+            results.push(node.data);
+            if (node.left) _walk(node.left);
+            if (node.right) _walk(node.right);
+        }
+        _walk(this.root);
+        console.log(results);
+        return results;
     }
     inorder() {
         let results = [];
-        let _move = (node) => {
-            if (node.left) _move(node.left);
-            results.push(node.value);
-            if (node.right) _move(node.right);
-
+        const _walk = (node) => {
+            if (node.left) _walk(node.left);
+            results.push(node.data);
+            if (node.right) _walk(node.right);
         }
-        _move(this.root);
-        return results
-
+        _walk(this.root);
+        console.log(results);
+        return results;
     }
     postorder() {
         let results = [];
-        let _move = (node) => {
-            if (node.left) _move(node.right);
-            if (node.right) _move(node.node);
-            results.push(node.value);
+        const _walk = (node) => {
+            if (node.left) _walk(node.left);
+            if (node.right) _walk(node.right);
+            results.push(node.data);
         }
-        _move(this.root);
-        return results
-
+        _walk(this.root);
+        console.log(results);
+        return results;
     }
-    add(Nodeitem) {
-        let current = this.root;
-        //create a new node
-        let newnode = new Node(Nodeitem);
-        //if we didnt have any root we put the new node as a root
-        if (!this.root) {
-            this.root = newnode;
+    findmaximumvalue() {
+        let curent = this.root;
+        while (curent.right) {
+            curent = curent.right;
         }
-        else {
-            while (current) {
-                if (newnode.value < current.value) {
-                    if (!current.left) {
-                        current.left = newnode;
-                        break;
-                    }
-                    //to pass all the left side of the tree
-                    current = current.left;
-                }
-                else if (newnode.value > current.value) {
-                    if (!current.right) {
-                        current.right = newnode;
-                        break;
-                    }
-                    //to pass all the right side of the tree
-                    current = current.right;
-                }
-                else {
-                    break;
-                }
-            }
-        }
+        return curent.data;
     }
-    contain(Nodeitem) {
-        let current = this.root;
-        if (!this.root) {
-            return false;
-        }
-        else if (this.root.value === Nodeitem) {
-            return true;
-        }
-        while (current) {
-            if (Nodeitem < current.value) {
-                console.log("enterd");
-                if (!current.left) {
-                    return false;
-                }
-                if (current.left.value === Nodeitem) {
-                    return true;
-                }
-
-
-
-                current = current.left;
-            }
-            current = this.root;
-            while (current) {
-                if (Nodeitem > current.value) {
-                    if (!current.right) {
-                        return false;
-                    }
-                    if (current.right.value === Nodeitem) {
-                        return true
-                    }
-                    //to pass all the right side of the tree
-                    current = current.right;
-                }
-            }
-
-            return false;
-
-
-        }
-    }
- breadthFirstTraversal(tree) {
-    let resulte = [];
-    let childs = [];
-    let node = tree.root;
-    if (!node) return 'the tree is empty';
-    childs.push(node);
-    while (childs.length) {
-      let tempNode = childs.shift();
-      resulte.push(tempNode.value);
-      if (tempNode.left) childs.push(tempNode.left);
-      if (tempNode.right) childs.push(tempNode.right);
-    }
-    return resulte;
-  }
-  findmaximumvalue(node)
-  {
-    if (node == null) 
-    { 
-        return "the root value is null"; 
-    } 
-
-    let res = node.value; 
-    let lres =   this.findmaximumvalue(node.left); 
-    let  rres =  this.findmaximumvalue(node.right);
-
-    if (lres > res) 
-    { 
-        res = lres; 
-    } 
-    if (rres > res) 
-    { 
-        res = rres; 
-    } 
-    return res; 
-} 
 }
-  let tree = new BinaryTree();
-  tree.add(1);
-  tree.add(2);
-  tree.add(3);
-  tree.add(4);
-  tree.add(5);
-//   console.log(tree2.breadthFirstTraversal(tree1));
-// console.log(tree.contain(5));
-// console.log(tree.contain(3));
-// console.log(tree.contain(8));
-// console.log(tree.contain(10));
-// console.log(tree.contain(32));
-// console.log(tree.contain(15));
-// console.log(tree.contain(12));
-console.log(tree.findmaximumvalue(tree.root));
-// console.log(tree);
+let bts = new BinaryTree(1);
+bts.insert(0);
+bts.insert(2);
+bts.insert(5);
+bts.insert(7);
+bts.insert(6);
+bts.insert(10);
+// console.log(bts.contain(8));
+// bts.preoder();
+// bts.postorder();
+console.log(bts.findmaximumvalue());
+// console.log(bts);
